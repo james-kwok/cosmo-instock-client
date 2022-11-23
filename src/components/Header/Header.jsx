fbc9313687c37e122ebb7cad8951908ff59400a2
Yu Wang <yuwangconductor@gmail.com>
2022-11-23 17:10:11 -0500

    header finally done omg

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,22 +7,18 @@ function Header (){
     return(
         <header className="header">
             <div className="header__logo">
-                <Link to="/warehouses">
-                    <img src={logo} alt="our logo"/>
+                <Link to="/warehouses" className="header__link">
+                    <img className="header__img" src={logo} alt="our logo"/>
                 </Link>
             </div>
             
             <nav className="header__nav">
-                <ul>
-                    <li>
-                        <NavLink to="/warehouses">
-                            <p>Warehouses</p>
-                        </NavLink>
+                <ul className="header__list">
+                    <li className="header__item">
+                        <NavLink to="/warehouses" className="header__navlink">Warehouses</NavLink>
                     </li>
-                    <li>
-                        <NavLink to="/inventory">
-                            <p>Inventory</p>
-                        </NavLink>
+                    <li className="header__item">
+                        <NavLink to="/inventory" className="header__navlink">Inventory</NavLink>
                     </li>
                 </ul>
             </nav>

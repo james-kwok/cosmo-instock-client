42ef038df84dce730c0f6221d228c3119c096e09
Michael Shimeles <michaelwasihun96@gmail.com>
2022-11-24 13:09:18 -0500

    Fixed a bug that when warehouses were all deleted loading component would show

--- src/components/WarehouseList/WarehouseList.jsx
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -10,9 +10,9 @@ import Modal from "../Modal/Modal";
 const WarehouseList = () => {
   const getURL = "http://localhost:8080/api/warehouses";
 
-  const [ warehouses, setWarehouses ] = useState([]);
-  const [ showModal, setShowModal ] = useState(false);
-  const [ toDelete, setToDelete ] = useState(null)
+  const [warehouses, setWarehouses] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [toDelete, setToDelete] = useState(null);
   useEffect(() => {
     axios
       .get(getURL)
@@ -26,22 +26,84 @@ const WarehouseList = () => {
   }, [showModal]);
 
   const modalHandler = (warehouse, modal) => {
-    console.log(warehouse)
-    setShowModal(modal)
-    setToDelete(warehouse)
+    console.log(warehouse);
+    setShowModal(modal);
+    setToDelete(warehouse);
   };
 
   const clickedInfo = () => {
-    console.log("Clicked")
-  }
+    console.log("Clicked");
+  };
 
   if (warehouses.length === 0) {
-    return <h1>LOADING...</h1>;
+    return (
+      <div className="WarehouseList">
+        <div className="WarehouseList__container">
+          <h1 className="WarehouseList__title">Warehouses</h1>
+          <div className="WarehouseList__search">
+            <input
+              className="WarehouseList__input"
+              type="text"
+              placeholder="Search..."
+            />
+            <img
+              className="WarehouseList__search-icon"
+              src={searchIcon}
+              alt="static search icon"
+            />
+          </div>
+          <Link to="/warehouses/add" className="WarehouseList__button-add">
+            <div className="WarehouseList__button-wrapper">
+              <p className="WarehouseList__button-text">+ Add New Warehouse</p>
+            </div>
+          </Link>
+        </div>
+        <div className="WarehouseList__sort-row">
+          <div className="WarehouseList__sort-warehouse">
+            <span className="WarehouseList__sort-text">Warehouse</span>
+            <img
+              className="WarehouseList__sort-icon"
+              src={sortIcon}
+              alt="sorting icon"
+            />
+          </div>
+          <div className="WarehouseList__sort-address">
+            <span className="WarehouseList__sort-text">Address</span>
+            <img
+              className="WarehouseList__sort-icon"
+              src={sortIcon}
+              alt="sorting icon"
+            />
+          </div>
+          <div className="WarehouseList__sort-contact">
+            <span className="WarehouseList__sort-text">Contact Name</span>
+            <img
+              className="WarehouseList__sort-icon"
+              src={sortIcon}
+              alt="sorting icon"
+            />
+          </div>
+          <div className="WarehouseList__sort-contact-info">
+            <span className="WarehouseList__sort-text">
+              Contact Information
+            </span>
+            <img
+              className="WarehouseList__sort-icon"
+              src={sortIcon}
+              alt="sorting icon"
+            />
+          </div>
+          <div className="WarehouseList__sort-action">
+            <span className="WarehouseList__sort-action-text">Actions</span>
+          </div>
+        </div>
+      </div>
+    );
   }
   return (
     <>
       {showModal ? (
-        <Modal modalHandler={modalHandler} warehouse={toDelete}/>
+        <Modal modalHandler={modalHandler} warehouse={toDelete} />
       ) : (
         <div className="WarehouseList">
           <div className="WarehouseList__container">

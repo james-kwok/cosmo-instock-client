4b9b1d172ae5f7f0beeb060dcd5f0213a46f9807
ruoxinhe <ruoxin.he@mail.utoronto.ca>
2022-11-24 20:56:37 -0500

    added initial warehouse related inventory items

--- src/components/WarehouseInventory/WarehouseInventory.jsx
+++ src/components/WarehouseInventory/WarehouseInventory.jsx
@@ -1,12 +1,13 @@
 import "./WarehouseInventory.scss";
-import InventoryItem from "../InventoryItem/InventoryItem";
+import WarehouseInventoryItem from "../WarehouseInventoryItem/WarehouseInventoryItem";
 import { Link } from "react-router-dom";
 import sortIcon from "../../assets/icons/sort-24px.svg";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 const WarehouseInventory = () => {
-  const getURL = "http://localhost:8080/api/warehouses/:id";
+  const getURL =
+    "http://localhost:8080/api/warehouses/2922c286-16cd-4d43-ab98-c79f698aeab0";
   const [warehouseinventory, setWarehouseinventory] = useState([]);
   useEffect(() => {
     axios
@@ -21,66 +22,55 @@ const WarehouseInventory = () => {
   if (warehouseinventory.length === 0) {
     return <h1>LOADING...</h1>;
   }
-  console.log(warehouseinventory);
   return (
-    <div className="inventory-list__sort-row">
-      <div className="inventory-list__sort-inventory">
-        <span className="inventory-list__sort-text">INVENTORY ITEM</span>
-        <img
-          className="inventory-list__sort-icon"
-          src={sortIcon}
-          alt="sorting icon"
-        />
+    <div className="inventory-list">
+      <div className="inventory-list__sort-row">
+        <div className="inventory-list__sort-inventory">
+          <span className="inventory-list__sort-text">INVENTORY ITEM</span>
+          <img
+            className="inventory-list__sort-icon"
+            src={sortIcon}
+            alt="sorting icon"
+          />
+        </div>
+        <div className="inventory-list__sort-category">
+          <span className="inventory-list__sort-text">CATEGORY</span>
+          <img
+            className="inventory-list__sort-icon"
+            src={sortIcon}
+            alt="sorting icon"
+          />
+        </div>
+        <div className="inventory-list__sort-status">
+          <span className="inventory-list__sort-text">STATUS</span>
+          <img
+            className="inventory-list__sort-icon"
+            src={sortIcon}
+            alt="sorting icon"
+          />
+        </div>
+        <div className="inventory-list__sort-quant">
+          <span className="inventory-list__sort-text">QTY</span>
+          <img
+            className="inventory-list__sort-icon"
+            src={sortIcon}
+            alt="sorting icon"
+          />
+        </div>
+        <div className="inventory-list__sort-action">
+          <span className="inventory-list__sort-action-text">ACTIONS</span>
+        </div>
       </div>
-      <div className="inventory-list__sort-category">
-        <span className="inventory-list__sort-text">CATEGORY</span>
-        <img
-          className="inventory-list__sort-icon"
-          src={sortIcon}
-          alt="sorting icon"
-        />
-      </div>
-      <div className="inventory-list__sort-status">
-        <span className="inventory-list__sort-text">STATUS</span>
-        <img
-          className="inventory-list__sort-icon"
-          src={sortIcon}
-          alt="sorting icon"
-        />
-      </div>
-      <div className="inventory-list__sort-quant">
-        <span className="inventory-list__sort-text">QTY</span>
-        <img
-          className="inventory-list__sort-icon"
-          src={sortIcon}
-          alt="sorting icon"
-        />
-      </div>
-      <div className="inventory-list__sort-warehouse">
-        <span className="inventory-list__sort-text">WAREHOUSE</span>
-        <img
-          className="inventory-list__sort-icon"
-          src={sortIcon}
-          alt="sorting icon"
-        />
-      </div>
-      <div className="inventory-list__sort-action">
-        <span className="inventory-list__sort-action-text">ACTIONS</span>
-      </div>
-      {/* {warehouseinventory.map((item) => {
+      {warehouseinventory.map((warehouse) => {
         return (
-          <InventoryItem
-            Inventory={item.item_name}
-            Category={item.category}
-            Status={item.status}
-            Quantity={item.quantity}
-            Warehouse={
-              warehouses.find((warehouse) => warehouse.id === item.warehouse_id)
-                .warehouse_name
-            }
+          <WarehouseInventoryItem
+            Inventory={warehouse.item_name}
+            Category={warehouse.category}
+            Status={warehouse.status}
+            Quantity={warehouse.quantity}
           />
         );
-      })} */}
+      })}
     </div>
   );
 };

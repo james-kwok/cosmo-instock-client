703abd49f7b9f660803fac8401356ac96f47b3d6
Michael Shimeles <michaelwasihun96@gmail.com>
2022-11-27 13:44:59 -0500

    Up to date develop

---
+++ src/components/InventoryModal/InventoryModal.jsx
@@ -0,0 +1,69 @@
+import "./InventoryModal.scss";
+import xIcon from "../../assets/icons/close-24px.svg";
+import axios from "axios";
+
+const Modal = ({ item, modalHandler }) => {
+  console.log("Item object", item);
+  const URL = "http://localhost:8080/api/inventories";
+  const id = item.id;
+  const handleDelete = () => {
+    axios
+      .delete(`${URL}/${id}`)
+      .then((response) => {
+        console.log("Deleted inventory" + response);
+        modalHandler(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <>
+      <div className="modal">
+        <div className="modal__container">
+          <div className="modal__container-text">
+            <div className="modal__close">
+              <img
+                src={xIcon}
+                className="modal__close-img"
+                alt="closing icon"
+                onClick={() => {
+                  modalHandler(false);
+                }}
+              />
+            </div>
+            <div className="modal__header">
+              <h4 className="modal__title">
+                Delete {item.item_name} inventory item?
+              </h4>
+            </div>
+            <div className="modal__description">
+              <p className="modal__description-text">
+                Please confirm that you'd like to delete the {item.item_name}{" "}
+                from the inventory list. You won't be able to undo this action.
+              </p>
+            </div>
+          </div>
+          <div className="modal__container-cta">
+            <div className="modal__cta">
+              <button
+                onClick={() => {
+                  modalHandler(false);
+                }}
+                className="modal__cta-cancel"
+              >
+                Cancel
+              </button>
+              <button onClick={handleDelete} className="modal__cta-delete">
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Modal;
